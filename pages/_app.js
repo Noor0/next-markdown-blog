4511fa192644f1794c011b10d9aa1ab81852f004
Noor ul haq <noor@pickfu.com>
2021-12-16 18:06:44 +0500

    integrate google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
+import Script from "next/script"
 import Header from '../components/Header'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-NGFY7CX3TM" />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{__html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-NGFY7CX3TM');
+        `}}
+      />
       <Header />
       <main className='container'>
         <Component {...pageProps} />
