6c263439f4db5de6070708a2f5f6fff2ec331ebb
Noor ul haq <noor@pickfu.com>
2021-12-20 16:03:07 +0500

    add google optemize script

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-W9VWRHKQ74" />
+      <Script strategy="afterInteractive" src="https://www.googleoptimize.com/optimize.js?id=OPT-5CNL7DD" />
       <Script
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{__html: `
