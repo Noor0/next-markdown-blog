7a372f8c89747e2d7c854844e53d7ac55c5b9f71
Noor ul haq <noor@pickfu.com>
2022-02-21 16:01:38 +0500

    log button clicked event to GA

--- components/Header.js
+++ components/Header.js
@@ -3,11 +3,18 @@ import Link from 'next/link'
 export default function Header() {
   return (
     <header>
-      <div className='container'>
-        <Link href='/' passHref>
+      <div className="container">
+        <Link href="/" passHref>
           <h2>Dev Blog</h2>
         </Link>
+        <button
+          onClick={() => {
+            window.gtag("event", "amazing_button_clicked");
+          }}
+        >
+          amazing button
+        </button>
       </div>
     </header>
-  )
+  );
 }
