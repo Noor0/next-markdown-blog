4793a4af67d73df2dfab16c1640ca3b46dfc8a33
Noor ul haq <noor@pickfu.com>
2022-02-21 19:29:21 +0500

    log page_view event on route change

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,28 @@
+import React from "react"
 import Script from "next/script"
+import { useRouter } from 'next/router'
+
 import Header from '../components/Header'
 import '../styles/globals.css'
 
+const handleRouteChange = (url, opts) => {
+  if (!process.browser) return;
+  if (!window.gtag) console.error("gtag not found")
+  window.gtag("event", "page_view", {
+    page_path: url,
+    page_title: document.title,
+    page_location: window.location.href,
+  });
+}
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  React.useEffect(() => {
+    router.events.on("routeChangeComplete", handleRouteChange)
+    return () => router.events.off("routeChangeComplete", handleRouteChange)
+  }, [router.events])
+
   return (
     <>
       <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-W9VWRHKQ74" />
